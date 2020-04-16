9a1c2191e924d625a76f993be18713d8946b24ff
bautheac <o.bautheac@gmail.com>
2020-04-16 17:36:06 +0100

    Updates

---
+++ precache-manifest.b4d265df19d929796e72467d7faeca60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd15cbc35e72699a36abc0cc5f969a5c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3b436e49dc9bb249ac5d",
+    "url": "/monsters-rolodex/static/css/main.cc90f22c.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b436e49dc9bb249ac5d",
+    "url": "/monsters-rolodex/static/js/main.0d429171.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
