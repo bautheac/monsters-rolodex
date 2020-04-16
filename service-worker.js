136d31efcd7def4b19a1e4b6469710a28a590d11
bautheac <o.bautheac@gmail.com>
2020-04-16 15:41:54 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/https/bautheac/github.io/monsters-rolodex/precache-manifest.51be4a5c10d992707024e029704bc337.js"
+  "/https//bautheac/github.io/monsters-rolodex/precache-manifest.d37e5329a0767e2ebcf3bd27ca7df545.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https/bautheac/github.io/monsters-rolodex/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https//bautheac/github.io/monsters-rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
